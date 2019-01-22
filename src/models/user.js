4ab5607cd3f079353f731b7398a40b0ef256793a
GeorgiaWebantic <georgia@webantic.co.uk>
2019-01-22 20:50:05 +0000

    update user model

--- src/models/user.js
+++ src/models/user.js
@@ -23,7 +23,7 @@ userSchema.pre('save', function hashPassword(next) {
 
 userSchema.methods.validatePassword = function validatePassword(password) {
   return bcrypt.compareSync(password, this.password);
-}
+};
 
 const User =  mongoose.model('User', userSchema);
 
